0ed4511ead6864883cf9226e6a676cfd21a13f06
Michael Steinberg <steinberg.michael@outlook.com>
2013-08-18 21:39:45 -0700

    scope selector 3/4 done
    checking a parent will select all children through all generations
    checking a child will update parents all the way back to epoch
    unchecking a parent will uncheck all children
    unchecking a child will not uncheck a parent, but will uncheck it's
    children
    
    a list displays with all selected scopes
    
    todo
    turn list into something pretty
    pretty solution should have the ability to remove scope choices

--- BCWeb/BCWeb/Scripts/Areas/Account/ScopesList.js
+++ BCWeb/BCWeb/Scripts/Areas/Account/ScopesList.js
@@ -15,28 +15,18 @@ var app = angular.module('scopePicker', ['filters']).controller('ScopesCtrl', fu
 
     $scope.t1Expand = function (value) {
         $scope.t1Parent = value;
-        console.log('t1 choice: ' + value);
     };
 
     $scope.t2Expand = function (value) {
         $scope.t2Parent = value;
-        console.log('t2 choice: ' + value);
     };
 
-    $scope.foo = function (value) {
-        console.log('foo : ' + value);
-    };
 
-    $scope.thing = function (data) {
-        var found = false;
-        angular.forEach($scope.selectedScopes, function (v, k) {
-            if (v.Id === data.Id) {
-                found = true;
-                v.checked = data.checked;
-                return;
-            }
-        });
-
+    $scope.changeScopeSelection = function (data) {
+        var found = $scope.updateScopeIfInSelection(data);
+    
+        // if first selection or if a match was not found, 
+        // add a new selection to the array
         if ($scope.selectedScopes.length === 0 || !found) {
             $scope.selectedScopes.push({
                 Id: data.Id,
@@ -45,9 +35,67 @@ var app = angular.module('scopePicker', ['filters']).controller('ScopesCtrl', fu
             });
         }
 
-        console.log(data);
+
+        $scope.updateParent(data);
+
+        $scope.updateChildren(data);
     };
 
+
+    // recursive function that goes through the lineage until epoch
+    $scope.updateParent = function (aScope) {
+        angular.forEach($scope.Scopes, function (v, k) {
+            // if found parent and this is checked, update parent
+            if (v.Id === aScope.ParentId && aScope.checked) {
+                v.checked = true;
+                // recurse through lineage until epoch;
+                $scope.updateParent(v);
+                // if parent was selected before, then update, else push onto array
+                if (!$scope.updateScopeIfInSelection(v)) {
+                    $scope.selectedScopes.push({
+                        Id: v.Id,
+                        Desc: v.Description,
+                        checked: v.checked
+                    });
+                }
+            }
+        });
+    };
+
+    // recursive function that goes through all offspring over n generatoins
+    $scope.updateChildren = function (aScope) {
+        angular.forEach($scope.Scopes, function (v, k) {
+            // if found child update children, check or uncheck
+            if (v.ParentId === aScope.Id) {
+                v.checked = aScope.checked;
+                // recurse through children
+                $scope.updateChildren(v);
+                // if child has been selected before, then update, else push onto array
+                if (!$scope.updateScopeIfInSelection(v)) {
+                    $scope.selectedScopes.push({
+                        Id: v.Id,
+                        Desc: v.Description,
+                        checked: v.checked
+                    });
+                }
+            }
+        });
+    };
+
+    // returns true if found update, returns false if not found
+    $scope.updateScopeIfInSelection = function (aScope) {
+        var found = false;
+        // loop through selected scopes
+        angular.forEach($scope.selectedScopes, function (v, k) {
+            // if match found for data
+            if (v.Id === aScope.Id) {
+                found = true;
+                v.checked = aScope.checked;
+                return;
+            }
+        });
+        return found;
+    };
 });
 
 // usage: ng-repeat="foo in bar | parentIdEqual: {{thing}}
