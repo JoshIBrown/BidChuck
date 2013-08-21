eb2694e0f9ebc9cf85af4ad32fc7d3ab39266aea
Michael Steinberg <steinberg.michael@outlook.com>
2013-08-21 15:26:21 -0700

    database migration for state/county is working
    database migration pulls seed data from csv files to load state and
    county tables

--- BCWeb/BCModel/Migrations/Configuration.cs
+++ BCWeb/BCModel/Migrations/Configuration.cs
@@ -1,8 +1,10 @@
 namespace BCModel.Migrations
 {
     using System;
+    using System.Data;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.OleDb;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BCModel.BidChuckContext>
@@ -26,6 +28,55 @@ namespace BCModel.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            DbSet<State> states = context.State;
+            DbSet<County> counties = context.Counties;
+            string path = AppDomain.CurrentDomain.BaseDirectory.Replace("BCWeb\\bin\\",string.Empty) + "BCModel\\Migrations\\";
+            string connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='text;HDR=Yes';", path);
+            string commandText = "select [ID],[Code],[Name] from state.csv";
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                using (OleDbDataAdapter adap = new OleDbDataAdapter(commandText, conn))
+                {
+                    conn.Open();
+                    using (DataSet ds = new DataSet())
+                    {
+                        adap.Fill(ds);
+                        foreach (DataTable table in ds.Tables)
+                        {
+                            foreach (DataRow row in table.Rows)
+                            {
+                                State toAdd = new State { Id = int.Parse(row["ID"].ToString()), Abbr = row["Code"].ToString(), Name = row["Name"].ToString() };
+                                states.AddOrUpdate(toAdd);
+                            }
+                        }
+                    }
+                }
+            }
+            context.SaveChanges();
+            commandText = "select [ID],[StateID],[Name] from county.csv";
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                using (OleDbDataAdapter adap = new OleDbDataAdapter(commandText, conn))
+                {
+                    conn.Open();
+                    using (DataSet ds = new DataSet())
+                    {
+                        adap.Fill(ds);
+                        foreach (DataTable table in ds.Tables)
+                        {
+                            foreach (DataRow row in table.Rows)
+                            {
+                                County toAdd = new County { Id = int.Parse(row["ID"].ToString()), StateId = int.Parse(row["StateID"].ToString()), Name = row["Name"].ToString() };
+                                counties.AddOrUpdate(toAdd);
+                            }
+                        }
+                    }
+                }
+            }
+            context.SaveChanges();
+
         }
     }
 }
